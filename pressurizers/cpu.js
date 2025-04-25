2c16709f555be1d6d6b873fd01b1604e2e852830
Alexey Kazakov <alkazako@redhat.com>
2025-04-25 11:23:15 -0700

    more

--- pressurizers/cpu.js
+++ pressurizers/cpu.js
@@ -26,9 +26,9 @@ const pressurizeCpu = async () => {
                 });
         }
         i++;
-        // Add a 10ms pause every 1000 iterations
+        // Add a 100ms pause every 1000 iterations
         if(i % 1000 === 0) {
-            await new Promise(resolve => setTimeout(resolve, 10));
+            await new Promise(resolve => setTimeout(resolve, 100));
         }
     }
 }
